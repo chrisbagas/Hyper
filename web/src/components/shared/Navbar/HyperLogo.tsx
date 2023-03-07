00b73a522f7022d85439c16f64d732c7335ec1f2
Adrian Ardizza <tmgfaction@gmail.com>
2023-03-07 13:59:22 +0700

    [CHORE] Make sidebar logo clickable

--- web/src/components/shared/Navbar/HyperLogo.tsx
+++ web/src/components/shared/Navbar/HyperLogo.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link"
 import React from "react"
 
 export const HyperLogo: React.FC = () => (
   <div className="flex justify-center w-full">
-    <img src="/hyper-logo.svg" className="w-12" />
+    <Link href="/" className="hover:scale-[1.10] active:scale-95 transition-all">
+      <img src="/hyper-logo.svg" className="w-12" />
+    </Link>
   </div>
 ) 
