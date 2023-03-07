bbc8dbcd7c0cf358c8b53bc64c6bad467831d994
stndaru <stefanus.ndaru@ui.ac.id>
2023-03-07 20:01:42 +0700

    [RED] Create test for games and gameservice

--- web/src/server/api/routers/game.ts
+++ web/src/server/api/routers/game.ts
@@ -1,8 +1,17 @@
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import { GameService } from "../services/GameService";
+import { z } from "zod";
 
 export const gameRouter = createTRPCRouter({
   getAll: publicProcedure
-    .query(({ ctx }) => {
-      return ctx.prisma.game.findMany()
+    .query(async ({ ctx }) => {
+      const games = await GameService.getAllGames(ctx.prisma)
+      return games
+    }),
+  getById: publicProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      const game = await GameService.getById(input, ctx.prisma)
+      return game
     })
 })
