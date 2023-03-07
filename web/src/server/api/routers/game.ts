6eb0f90870a7a4204b1dfa0c01af99fed1789a12
stndaru <stefanus.ndaru@ui.ac.id>
2023-03-07 20:46:03 +0700

    [GREEN] Add GameService router and getByID test

--- web/src/server/api/routers/game.ts
+++ web/src/server/api/routers/game.ts
@@ -3,15 +3,17 @@ import { GameService } from "../services/GameService";
 import { z } from "zod";
 
 export const gameRouter = createTRPCRouter({
+  
   getAll: publicProcedure
     .query(async ({ ctx }) => {
-      const games = await GameService.getAllGames(ctx.prisma)
+        const games = await GameService.getAllGames(ctx.prisma)
       return games
     }),
+
   getById: publicProcedure
     .input(z.string())
     .query(async ({ ctx, input }) => {
-      const game = await GameService.getById(input, ctx.prisma)
+        const game = await GameService.getById(input, ctx.prisma)
       return game
     })
 })
