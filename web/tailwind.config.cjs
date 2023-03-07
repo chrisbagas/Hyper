9f76d46af91b1332607708f2c740060d77f8acab
Adrian Ardizza <tmgfaction@gmail.com>
2023-03-07 15:29:36 +0700

    [CHORE] Add Rubik font to project

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        mono: "'Roboto Mono', monospace"
+        "sans": ["Rubik", "sans-serif"]
       },
       colors: {
         base: {
