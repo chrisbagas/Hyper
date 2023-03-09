2e8095f382ce1323a43f4bc8bdc08f104f5811c0
Julius Prayoga Raka Nugroho <raka.gundam@hotmail.com>
2023-03-09 22:00:14 +0700

    [REFACTOR] Clean PlayerStatisticsService smell

--- web/src/server/api/services/PlayerStatisticsService.ts
+++ web/src/server/api/services/PlayerStatisticsService.ts
@@ -1,25 +1,24 @@
 export class PlayerStatisticsService {
-    public static async valorantAPI(link:String) {
+    public static async valorantAPI(link: string) {
         const res = await fetch(
             "https://api.henrikdev.xyz/valorant/" + link
         );
-        const data = await res.json();
-        return data;
-    };
+        return res.json();
+    }
 
     public static async getValorantAccountData(username: any, tagline: any) {
         return PlayerStatisticsService.valorantAPI(`v1/account/:${username}/:${tagline}`)
-    };
+    }
 
     public static async  getValorantMMRData(username: any, tagline: any) {
         return PlayerStatisticsService.valorantAPI(`v2/mmr/ap/:${username}/:${tagline}`)
-    };
+    }
 
     public static async  getValorantMMRHistory(username: any, tagline: any) {
         return PlayerStatisticsService.valorantAPI(`v1/mmr-history/ap/:${username}/:${tagline}`)
-    };
+    }
 
     public static async  getValorantMatchHistory(username: any, tagline: any) {
         return PlayerStatisticsService.valorantAPI(`v3/matches/ap/:${username}/:${tagline}`)
-    };
+    }
 }
