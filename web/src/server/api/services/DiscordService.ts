9313a2d6f8f5a9515dabd85d7bedc3a2305e0eb8
Adrian Ardizza <tmgfaction@gmail.com>
2023-03-16 21:13:52 +0700

    [REFACTOR] Refactor promise return on DiscordService

--- web/src/server/api/services/DiscordService.ts
+++ web/src/server/api/services/DiscordService.ts
@@ -23,27 +23,23 @@ export class DiscordService {
 
   public static async getUserDetails(userId: string, prisma: PrismaClient): Promise<DiscordAccount> {
     const accessToken = await this.getDiscordToken(userId, prisma);
-    const userData = await this.fetchDiscord("/users/@me", {
+    return this.fetchDiscord("/users/@me", {
       method: "GET",
       headers: {
         "Authorization": "Bearer " + accessToken,
       }
     })
       .then((res) => res.json());
-
-    return userData;
   }
 
   public static async getUserConnections(userId: string, prisma: PrismaClient): Promise<DiscordConnections> {
     const accessToken = await this.getDiscordToken(userId, prisma);
-    const connections = await this.fetchDiscord("/users/@me/connections", {
+    return this.fetchDiscord("/users/@me/connections", {
       method: "GET",
       headers: {
         "Authorization": "Bearer " + accessToken,
       }
     })
       .then((res) => res.json());
-
-    return connections;
   }
 }
