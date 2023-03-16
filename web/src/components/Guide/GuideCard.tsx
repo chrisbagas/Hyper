a8beda934ba82a4a0156cbb26fa7f3e66f66f4e4
Christopher Bagas Laiputra <christopher.bagas@ui.ac.id>
2023-03-16 22:20:24 +0700

    CHORE: Added status badge on guide card

--- web/src/components/Guide/GuideCard.tsx
+++ web/src/components/Guide/GuideCard.tsx
@@ -5,13 +5,21 @@ export interface GuideCardProps{
     username?:string
     createdAt:Date
     content?:string
+    status:string
 }
 
-export const GuideCard: React.FC<GuideCardProps> = ({ title,username,createdAt,content }) => (
+export const GuideCard: React.FC<GuideCardProps> = ({ title,username,createdAt,content,status }) => (
     <div className="card w-full bg-base-100 shadow-xl">
         <figure><img src="https://dafunda.com/wp-content/uploads/2021/03/CSGO-steam-terhapus.jpg" alt="Shoes" /></figure>
         <div className="card-body">
-            <div className="badge badge-warning">Guide</div>
+            <div>
+                {status === "PUBLISHED"?(
+                    <div className="badge badge-success mr-2">{status}</div>
+                ):(
+                    <div className="badge mr-2">{status}</div>
+                )}
+                <div className="badge badge-warning">Guide</div>
+            </div>
             <h2 className="card-title">
                 {title}
             </h2>
