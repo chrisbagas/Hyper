dd05a6984a243700d55ab59457287a022c1bded6
Julius Prayoga Raka Nugroho <raka.gundam@hotmail.com>
2023-03-22 21:35:09 +0700

    [GREEN] Modified main Valorant data service

--- web/src/server/api/services/PlayerStatisticsService.ts
+++ web/src/server/api/services/PlayerStatisticsService.ts
@@ -26,6 +26,7 @@ export class PlayerStatisticsService {
         const accountData = await this.getValorantAccountData(username, tagline)
         const mmrData = await this.getValorantMMRData(username, tagline)
         const mmrHistory = await this.getValorantMMRHistory(username, tagline)
+        const matchHistory = await this.getValorantMatchHistory(username, tagline)
 
         const valorantData: any = {
             "status":-1,
@@ -40,9 +41,13 @@ export class PlayerStatisticsService {
                 "elo":0,
                 "rank_image":""
             },
-            "mmrHistory": [
+            "competitiveHistory": [
                 
             ]
+        }
+
+        if (accountData.status == 200 && mmrData.status == 200 && mmrHistory.status == 200 && matchHistory.status == 200) {
+            valorantData.status = 200
         }
 
         valorantData.accountData.region = accountData.data.region
@@ -55,15 +60,34 @@ export class PlayerStatisticsService {
         valorantData.mmrData.rank_image = mmrData.data.current_data.images.large
 
         var counter = 0
-        for (var i in mmrHistory.data) {
+        for (let i = 0; i < 5; i++) {
             if (counter == 5) break
 
             var item = mmrHistory.data[i];
+            var item2 = matchHistory.data[i].players.all_players;
 
-            valorantData.mmrHistory.push({
+            for (let j = 0; j < 10; j++) {
+                if (item2[j].name === username && item2[j].tag === tagline) {
+                    item2 = item2[j]
+                    break
+                }
+            }
+
+            var victory_boolean = ""
+            if (item.mmr_change_to_last_game > 0) {
+                victory_boolean = "true"
+            } else victory_boolean = "false"
+
+            valorantData.competitiveHistory.push({
                 "rank": item.currenttierpatched,
                 "rank_image": item.images.large,
-                "elo_change": item.mmr_change_to_last_game
+                "elo_change": item.mmr_change_to_last_game,
+                "kills": item2.stats.kills,
+                "deaths": item2.stats.deaths,
+                "assists": item2.stats.assists,
+                "headshot_rate": Math.round(item2.stats.headshots / (item2.stats.headshots + item2.stats.bodyshots + item2.stats.legshots) * 100),
+                "agent_image": item2.assets.agent.small,
+                "victory": victory_boolean
             });
 
             counter++
