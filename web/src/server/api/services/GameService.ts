e51bdf866b86a6e61fa8a5d97869b197f695912f
Stefanus Ndaru Wedhatama <stefanus.ndaru@ui.ac.id>
2023-03-22 13:26:30 +0700

    HYP-21 (Dashboard - Main Landing Page) MR2

--- web/src/server/api/services/GameService.ts
+++ web/src/server/api/services/GameService.ts
@@ -1,4 +1,4 @@
-import type { Game, PrismaClient } from "@prisma/client";
+import type { PrismaClient } from "@prisma/client";
 
 export class GameService {
   static getAllGames(prisma: PrismaClient) {
