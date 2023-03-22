d4d57201ccaeba01e6e98679cdde714c749f7063
Adrian Ardizza <tmgfaction@gmail.com>
2023-03-22 14:15:54 +0700

    [CHORE] Setup app skeleton for Discord integration

--- discord/src/engines/discord/bot.ts
+++ discord/src/engines/discord/bot.ts
@@ -1,4 +1,7 @@
 import { Client, GatewayIntentBits } from "discord.js"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const discordBotClient = new Client({
   intents: [
--- discord/src/server.ts
+++ discord/src/server.ts
@@ -1,12 +1,16 @@
 import express from "express"
+import dotenv from "dotenv"
+import discordBotClient from "./engines/discord/bot"
+
+dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3001
 
-app.get("/", (_, res) => {
-  res.send("Hello, world!")
-})
-
 app.listen(port, () => {
   console.log(`Discord service is listening on port ${port}`)
+})
+
+discordBotClient.on("ready", () => {
+  console.log("Bot is ready")
 })
