0a26ad9fddb065251b0fb075120dc8bcc9847f35
Muhammad Asyraf <muhammad.asyraf@ui.ac.id>
2023-03-22 23:56:03 +0700

    [GREEN] create implementation for getConnectionAcc

--- web/src/server/api/routers/profileRouter.ts
+++ web/src/server/api/routers/profileRouter.ts
@@ -23,7 +23,11 @@ const profileRouter = createTRPCRouter({
       return ProfileService.getAllCountries(ctx.prisma)
     }),
 
-    
+    getConnectionAccount: protectedProcedure
+    .query(async ({ ctx }) => {
+      const res = await ProfileService.getConnectionAccount(ctx.session.user.id, ctx.prisma)
+      return res
+    }),
   
 });
 
