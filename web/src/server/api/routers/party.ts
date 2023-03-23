69cf669f1943f21b92ef6b12c8219fde345c0abd
Adrian Larry Ananda Sudarthio <adrian.larry@ui.ac.id>
2023-03-23 23:43:20 +0700

    Party System HYP-17

---
+++ web/src/server/api/routers/party.ts
@@ -0,0 +1,46 @@
+import { createTRPCRouter, publicProcedure} from "../trpc";
+import z from "zod";
+import { PartyType, PartyVisibility } from "@prisma/client";
+import { PartyService } from "../services/PartyService";
+
+
+export const partyRouter = createTRPCRouter({
+    getByGame: publicProcedure
+      .input(z.object({
+          id: z.string()
+      }))
+      .query(async ({ ctx, input }) => {
+          return PartyService.getParties(ctx.prisma, input.id);
+    }),
+  
+    createParty: publicProcedure
+      .input(z.object({
+        userId: z.string(),
+        gameId: z.string(),
+        partyTitle: z.string(),
+        partyType: z.nativeEnum(PartyType),
+        partyVisibility: z.nativeEnum(PartyVisibility)
+      }))
+      .mutation(async ({ ctx, input }) => {
+        return PartyService.createParty(ctx.prisma, input);
+    }),
+
+    joinParty: publicProcedure
+      .input(z.object({
+        userId: z.string(),
+        partyId: z.string(),
+        gameId: z.string()
+      }))
+      .mutation(async ({ ctx, input }) => {
+        return PartyService.joinParty(ctx.prisma, input)
+    }),
+
+    leaveParty: publicProcedure
+      .input(z.object({
+        userId: z.string(),
+        partyId: z.string()
+      }))
+      .mutation(async ({ ctx, input }) => {
+        return PartyService.leaveParty(ctx.prisma, input)
+    }),
+})
