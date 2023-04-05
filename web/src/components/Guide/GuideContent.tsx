ab04635bcb1ddd17b64a618bbb2aaf39fe4f6c2c
muhammad.kenshin <muhammad.kenshin@ui.ac.id>
2023-04-05 19:50:20 +0700

    [CHORE] Update markdown parser

--- web/src/components/Guide/GuideContent.tsx
+++ web/src/components/Guide/GuideContent.tsx
@@ -47,7 +47,7 @@ export const GuideContent: React.FC<GuideContentProps> = ({ type, title, content
             ol: ({node, depth, ...props}) => <ol className={`text-neutral-0 list-decimal list-inside ml-${depth*5}`} {...props} />,
             pre: ({node, ...props}) => <pre className="p-4 bg-base-1 text-base-5" {...props} />,
             blockquote: ({node, ...props}) => <blockquote className="p-4 my-4 border-l-4 border-gray-500 bg-gray-800" {...props}/>,
-            code: ({node, ...props}) => <code className="bg-base-2" {...props}/>
+            code: ({node, ...props}) => <code className="px-1 bg-base-2" {...props}/>
           }}>
           {content}
         </ReactMarkdown>
