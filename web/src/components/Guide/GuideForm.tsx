7b61328ff3f9dc65b712e3e5a40448ebafe1525c
muhammad.kenshin <muhammad.kenshin@ui.ac.id>
2023-04-13 15:27:57 +0700

    [REFACTOR] Polishing guide frontend

--- web/src/components/Guide/GuideForm.tsx
+++ web/src/components/Guide/GuideForm.tsx
@@ -1,7 +1,9 @@
 import { CommunityPostStatus, CommunityPostType, ContentType } from "@prisma/client"
 import React, { useState } from "react"
-import { EyeIcon, FolderPlusIcon, LinkIcon, PaperAirplaneIcon } from "@heroicons/react/24/solid"
+import { ExclamationTriangleIcon, LinkIcon, XCircleIcon } from "@heroicons/react/24/solid"
 import { GuideTopButtonGroup } from "./GuideTopButtonGroup"
+import { EyeIcon, FolderPlusIcon, PaperAirplaneIcon } from "@heroicons/react/24/outline"
+import { GuideConfirmationModal } from "./GuideConfirmationModal"
 
 export interface Post {
   type:CommunityPostType|undefined,
@@ -27,25 +29,25 @@ export const GuideForm: React.FC<GuideFormProps> = ({ postData, errorMessage, is
 
   return (
     <>
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-6 pb-16">
         
         <GuideTopButtonGroup className="px-16" returnUrl={postId ? `/${gameId}/your-guides/${postId}` : `/${gameId}/your-guides`}>
           <div className="flex justify-between gap-2">
-            <button className={`flex btn btn-ghost normal-case gap-2 ${isSubmitting && 'btn-disabled'}`} onClick={(e)=>onSubmit(e, CommunityPostStatus.DRAFT, false)}><FolderPlusIcon className="w-4"/> Save as Draft</button>
-            <button className={`flex btn btn-ghost normal-case gap-2 ${isSubmitting && 'btn-disabled'}`} onClick={(e)=>onSubmit(e, CommunityPostStatus.DRAFT, true)}><EyeIcon className="w-4"/> Save & Preview</button>
+            <button className={`flex btn btn-ghost normal-case gap-2 text-neutral-0 ${isSubmitting && 'btn-disabled'}`} onClick={(e)=>onSubmit(e, CommunityPostStatus.DRAFT, false)}><FolderPlusIcon className="w-4"/> Save as Draft</button>
+            <button className={`flex btn btn-ghost normal-case gap-2 text-neutral-0 ${isSubmitting && 'btn-disabled'}`} onClick={(e)=>onSubmit(e, CommunityPostStatus.DRAFT, true)}><EyeIcon className="w-4"/> Save & Preview</button>
             <button className={`flex btn btn-primary bg-primary-main border-primary-border hover:bg-primary-pressed hover:border-primary-pressed normal-case gap-2 ${isSubmitting && 'btn-disabled'}`} onClick={()=>setIsModalOpen(true)}><PaperAirplaneIcon className="w-4"/> Save & Publish Post</button>
           </div>
         </GuideTopButtonGroup>
 
         <div className="form-control w-full px-16">
           <label className="label">
-            <span className="label-text">Post Title</span>
+            <span className="label-text text-neutral-0">Post Title</span>
           </label>
           <input 
             type="text" 
             id="title"
             placeholder="How to Win a match" 
-            className="input input-bordered w-full" 
+            className="input input-bordered w-full bg-base-2 border-base-3 placeholder:text-base-4 text-neutral-0" 
             value={postData.title} 
             onChange={onChange}
           />
@@ -54,44 +56,44 @@ export const GuideForm: React.FC<GuideFormProps> = ({ postData, errorMessage, is
         <div className="flex justify-between px-16 gap-6">
           <div className="form-control w-1/4">
             <label className="label">
-              <span className="label-text">Header Attachment Type</span>
+              <span className="label-text text-neutral-0">Header Attachment Type</span>
             </label>
-            <select id="headerType" className="select w-full" value={postData.headerType} onChange={onChange}>
+            <select id="headerType" className={`select w-full bg-base-2 border-base-3 ${postData.headerType === undefined ? "text-base-4" : "text-neutral-0"}`} value={postData.headerType} onChange={onChange}>
               <option disabled selected value={undefined}>Select header type</option>
               <option value={ContentType.IMAGE}>Image</option>
               <option value={ContentType.VIDEO}>Video</option>
             </select>
             <label className="label">
-              <span className="label-text-alt">Please select the attachment type</span>
+              <span className="label-text-alt text-base-5">Please select the attachment type</span>
             </label>
           </div>
 
           <div className="form-control w-3/4">
             <label className="label">
-              <span className="label-text">Header Attachment Link</span>
+              <span className="label-text text-neutral-0">Header Attachment Link</span>
             </label>
             <label className="input-group">
-              <span><LinkIcon className="w-4"/></span>
+              <span className="bg-base-1"><LinkIcon className="w-4 text-base-4"/></span>
               <input 
                 type="text" 
                 id="headerUrl"
                 placeholder="www.medal.tv/001ojiad0983q09" 
-                className="input input-bordered w-full" 
+                className="input input-bordered w-full bg-base-2 border-base-3 placeholder:text-base-4 text-neutral-0" 
                 value={postData.headerUrl}
                 onChange={onChange}
               />
             </label>
             <label className="label">
-              <span className="label-text-alt">Only supports link or files provided by selected attachment type</span>
+              <span className="label-text-alt text-base-5">Only supports link or files provided by selected attachment type</span>
             </label>
           </div>
         </div>
 
         <div className="form-control w-full px-16">
           <label className="label">
-            <span className="label-text">Select Post Type</span>
+            <span className="label-text text-neutral-0">Select Post Type</span>
           </label>
-          <select id="type" className="select w-full" value={postData.type} onChange={onChange}>
+          <select id="type" className={`select w-full bg-base-2 border-base-3 ${postData.type === undefined ? "text-base-4" : "text-neutral-0"}`} value={postData.type} onChange={onChange}>
             <option disabled selected value={undefined}>Select post type</option>
             <option value={CommunityPostType.CLIP}>Clip</option>
             <option value={CommunityPostType.GUIDE}>Guide</option>
@@ -100,11 +102,11 @@ export const GuideForm: React.FC<GuideFormProps> = ({ postData, errorMessage, is
 
         <div className="form-control w-full px-16">
           <label className="label">
-            <span className="label-text">Post Content</span>
+            <span className="label-text text-neutral-0">Post Content</span>
           </label>
           <textarea 
             id="content"
-            className="textarea" 
+            className="textarea bg-base-2 border-base-3 placeholder:text-base-4 text-neutral-0" 
             placeholder="This post contains a match replay and ..."
             value={postData.content}
             onChange={onChange}
@@ -129,27 +131,24 @@ export const GuideForm: React.FC<GuideFormProps> = ({ postData, errorMessage, is
         </div>
       </div>
 
+      <GuideConfirmationModal
+        headerText="Save and publish post?"
+        contentText="You cannot make any more changes and this action is irreversible"
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+      >
+        <button 
+          className="btn btn-primary bg-primary-main border-primary-border hover:bg-primary-pressed hover:border-primary-pressed normal-case gap-2" 
+          onClick={(e)=>{
+            setIsModalOpen(false)
+            onSubmit(e, CommunityPostStatus.PUBLISHED, false)
+          }}
+        >
+          <PaperAirplaneIcon className="w-4"/> Save & Publish Post
+        </button>
+      </GuideConfirmationModal>
       
       
-      <input type="checkbox" id="my-modal" className="modal-toggle" />
-      <div className={`modal ${isModalOpen && 'modal-open'}`}>
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Save and publish post?</h3>
-          <p className="py-4">You cannot make any more changes and this action is irreversible</p>
-          <div className="modal-action">
-            <button className="btn btn-ghost normal-case gap-2" onClick={()=>setIsModalOpen(false)}>Go Back</button>
-            <button 
-              className="btn btn-primary bg-primary-main border-primary-border hover:bg-primary-pressed hover:border-primary-pressed normal-case gap-2" 
-              onClick={(e)=>{
-                setIsModalOpen(false)
-                onSubmit(e, CommunityPostStatus.PUBLISHED, false)
-              }}
-            >
-              <PaperAirplaneIcon className="w-4"/> Save & Publish Post
-            </button>
-          </div>
-        </div>
-      </div>
     </>
   )
 }
